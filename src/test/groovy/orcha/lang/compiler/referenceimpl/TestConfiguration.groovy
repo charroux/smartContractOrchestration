72b1d76a622ab7acab21a948e498cb32bb5937a8
charroux <benoit.charroux@free.fr>
2017-09-14 12:50:45 +0200

    update test

---
+++ src/test/groovy/orcha/lang/compiler/referenceimpl/TestConfiguration.groovy
@@ -0,0 +1,8 @@
+package orcha.lang.compiler.referenceimpl
+
+import org.springframework.boot.SpringBootConfiguration
+
+@SpringBootConfiguration
+class TestConfiguration implements PrepareOrderTestConfiguration, BenchmarkingExampleTestConfiguration{
+
+}
