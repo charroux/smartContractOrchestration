6e2c4347332d82a3ffeb8c54ecbce946938d15ee
charroux <benoit.charroux@free.fr>
2017-10-07 19:18:34 +0200

    event filtering can filter only one event

--- src/main/orcha/source/EventFilteringExample.groovy
+++ src/main/orcha/source/EventFilteringExample.groovy
@@ -1,6 +1,6 @@
 title "event filtering"
 
 receive event from eventFilteringInputFile condition "event == 0"
-compute eventFilteringCode1 with event.value
-when "eventFilteringCode1 terminates"
-send eventFilteringCode1.result to eventFilteringOutputFile
+compute eventFilteringCode with event.value
+when "eventFilteringCode terminates"
+send eventFilteringCode.result to eventFilteringOutputFile
